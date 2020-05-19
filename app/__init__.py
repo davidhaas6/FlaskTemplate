dbbc45522148fd509bd620caa3196ab56a4659e7
David Haas <davidhaas6@gmail.com>
2020-05-19 18:08:06 -0400

    debugging

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,6 @@ from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
 
 # Bootstrap
 from flask_bootstrap import Bootstrap
